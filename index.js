a9de582be2cb9f851002a0b56a9957dffa8fd264
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-03-26 00:22:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(HELLO) {
+    return HELLO.toLowerCase();
+}
+function logShout(string) {
+    return console.log(string.toUpperCase());
+}
+function logWhisper(HELLO) {
+    return console.log(HELLO.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if(string===string.toLowerCase())
+    return"I can't hear you!"
+    }
+    function sayHiToHeadphonedRoommate(string) {
+        if(string===string.toUpperCase())
+        return"YES INDEED!"
+    }  
+    function sayHiToHeadphonedRoommate(string) {
+        if(string===string.toLowerCase())
+        return'I can\'t hear you!'
+    }
+    function sayHiToHeadphonedRoommate(string) {
+        if(string==="Let's have dinner together!")
+        return"I would love to!"
+    }
